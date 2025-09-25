984e3bcd9f796da59f52dbd84171d976d0d02c03
Cristobal Reyes <cristobalreyes0420@gmail.com>
2025-09-25 17:49:02 +0200

    Agregada acción 'hola' en ApplicationController de nuevo por un pequeño error que no daba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
   def hola
     render html: "hola, mundo!"
   end
